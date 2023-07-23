f0fcbf7c2a869d6e1b61bc6873fb961bbd704dbf
Salman Zaheer <szahee2@students.towson.edu>
2023-07-23 18:02:23 -0400

    linked website

---
+++ app/components/BackTo.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function BackTo() {
+  return (
+    <Link
+      href="https://salmanzaheer.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+          />
+        </svg>
+      </button>
+    </Link>
+  );
+}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import ThemeButton from "./ThemeButton";
+import BackTo from "./BackTo";
 
 export default function Navbar() {
   return (
     <div className="max-w-3xl mx-auto px-4 lg:px-8">
       <div className="flex justify-between h-16">
         <div className="flex justify-between items-center w-full">
+          <BackTo />
+
           <Link href="/">
             <h1 className="text-2xl font-medium">
               Salman <span className="text-[#fca311]">Zaheer</span>
